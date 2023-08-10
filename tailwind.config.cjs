1a1f885eecd775f8d917dea82e6102395ce8e566
KDEV <lekietpro123@gmail.com>
2023-08-10 16:05:50 +0700

    add font Inter

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
@@ -11,11 +12,14 @@ module.exports = {
       colors: {
         'ct-green': '#00966B',
         'bg-container': '#141623'
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
       }
     }
   },
   plugins: [
-    plugin(function({addComponents, theme}) {
+    plugin(function ({ addComponents, theme }) {
       addComponents({
         '.container': {
           maxWidth: theme('columns.7xl'),
